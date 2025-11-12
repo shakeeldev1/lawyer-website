b27c7f6609e7a64ef3233ba178088739c3ca651e
Mogehs <mugheesurrehman06@gmail.com>
2025-11-12 17:13:54 +0500

    completed the lawyer apis

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 import authRoutes from "../features/auth/auth.routes.js";
 import secretaryRoutes from "../features/secretary/secretary.routes.js";
+import lawyerRoutes from "../features/lawyer/lawyer.routes.js";
 
 router.use("/auth", authRoutes);
 router.use("/secretary", secretaryRoutes);
+router.use("/lawyer", lawyerRoutes);
 
 export default router;
