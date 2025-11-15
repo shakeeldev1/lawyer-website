b2affe40a87ba1c12381c4a65cb6529192d03fe5
shakeel7521951 <shakeel7521951@gmail.com>
2025-11-15 13:40:44 +0500

    update backend url

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,6 +16,8 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 
+console.log("frontend url",process.env.FRONTEND_URL);
+
 app.use(cors({
     origin: process.env.FRONTEND_URL || 'http://localhost:5173',
     credentials: true,
