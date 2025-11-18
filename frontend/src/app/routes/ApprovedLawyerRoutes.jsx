8b3fe3b8b5fd0276a244f34b7d789868b81b544d
muqadashayyat8@gmail.com <muqadashayyat8@gmail.com>
2025-11-18 17:48:42 +0500

    update

--- frontend/src/app/routes/ApprovedLawyerRoutes.jsx
+++ frontend/src/app/routes/ApprovedLawyerRoutes.jsx
@@ -2,6 +2,7 @@
 import ApprovedLawyerNotifications from "../../features/approvedlawyer/pages/ApprovedLawyerNotifications";
 import ApprovedLawyerPage from "../../features/approvedlawyer/pages/ApprovedLawyerPage";
 import ApprovedLawyerLayout from "../layouts/ApprovedLawyerLayout";
+import RoleProtectedRoute from "../middlewares/RoleProtectedRoute";
 
 
 export const ApprovedLawyerRoutes = {
